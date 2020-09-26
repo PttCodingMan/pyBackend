9b0e2a68f65773fee967cd68a6d068a279888db2
MoonyHsiao <herrowei135@gmail.com>
2020-09-26 16:48:09 +0800

    feat(init): init

---
+++ src/config.py
@@ -0,0 +1,8 @@
+# try:
+#     from . import version
+# except ModuleNotFoundError:
+#     import version
+
+
+# class Config:
+#     Version = version.V
---
+++ src/main.py
@@ -0,0 +1,27 @@
+class Student:
+    def ReadYaml(self, x, y):
+        a = 10
+        b = 'test'
+        print(x)
+        print(y)
+        filePath = pathlib.Path(__file__).parent.absolute()
+        print('%s is %s' % ('this filePath', filePath))
+
+        yamlPath = str(filePath)+'/config/setting.yaml'
+
+        # with open(r'E:\data\fruits.yaml') as file:
+        with open(yamlPath) as file:
+            fruits_list = yaml.load(file, Loader=yaml.FullLoader)
+            print(fruits_list)
+
+
+if __name__ == '__main__':
+    import sys
+    import pathlib
+    import yaml
+    import version
+    # print("安安")
+    s = Student()
+    s.ReadYaml(3, 5)
+    aa = version.V
+    print(aa)
